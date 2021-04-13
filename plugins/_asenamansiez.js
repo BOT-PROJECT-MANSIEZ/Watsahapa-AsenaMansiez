fca055a6174be07694d145964e165d031c897f6e
BOT-PROJECT-MANSIEZ <81785721+BOT-PROJECT-MANSIEZ@users.noreply.github.com>
2021-04-14 06:20:16 +0700

    Update _asenamansiez.js

--- plugins/_asenamansiez.js
+++ plugins/_asenamansiez.js
@@ -15,7 +15,7 @@ const Lang = Language.getString('_asenamansiez');
 
 if (Config.WORKTYPE == 'private') {
 
-    Asena.addCommand({pattern: 'perintah ?(.*)', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
+    Asena.addCommand({pattern: 'asena ?(.*)', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
 
         var CMD_HELP = '';
         if (match[1] === '') {
@@ -83,7 +83,7 @@ if (Config.WORKTYPE == 'private') {
 }
 else if (Config.WORKTYPE == 'public') {
 
-    Asena.addCommand({pattern: 'perintah ?(.*)', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
+    Asena.addCommand({pattern: 'asena ?(.*)', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
 
         var CMD_HELP = '';
         if (match[1] === '') {
