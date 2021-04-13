fcb83ba5fc07226544b114b76c9a6890b10cd0be
BOT-PROJECT-MANSIEZ <81785721+BOT-PROJECT-MANSIEZ@users.noreply.github.com>
2021-04-14 00:06:57 +0700

    Update system_stats.js

--- plugins/system_stats.js
+++ plugins/system_stats.js
@@ -22,7 +22,7 @@ if (Config.WORKTYPE == 'private') {
     Asena.addCommand({pattern: 'alive', fromMe: true, desc: Lang.ALIVE_DESC}, (async (message, match) => {
 
         if (Config.ALIVEMSG == 'default') {
-            await message.client.sendMessage(message.jid,'```Tanrı Türk\'ü Korusun. 🐅 AsenaMansiez Rindu!```\n\n*Version:* ```'+Config.VERSION+'```\n*Branch:* ```'+Config.BRANCH+'```\n*Telegram Group:* https://t.me/petercord\n*Telegram Penulis:* https://t.me/diemmmmmmmmmm' , MessageType.text);
+            await message.client.sendMessage(message.jid,'```AKU ADALAH USERBOT . 🐅 ASENAMansiez Rindu ITU BERAT!```\n\n*Version:* ```'+Config.VERSION+'```\n*Branch:* ```'+Config.BRANCH+'```\n*Telegram Group:* https://t.me/petercord\n*Telegram Penulis:* https://t.me/diemmmmmmmmmm' , MessageType.text);
         }
         else {
             await message.client.sendMessage(message.jid,Config.ALIVEMSG + '\n*Powered by WhatsAsenaMansiez*', MessageType.text);
@@ -42,7 +42,7 @@ else if (Config.WORKTYPE == 'public') {
     Asena.addCommand({pattern: 'alive', fromMe: false, desc: Lang.ALIVE_DESC}, (async (message, match) => {
 
         if (Config.ALIVEMSG == 'default') {
-            await message.client.sendMessage(message.jid,'```Tanrı Türk\'ü Korusun. 🐺 Asena Hizmetinde!```\n\n*Version:* ```'+Config.VERSION+'```\n*Branch:* ```'+Config.BRANCH+'```\n*Telegram Group:* https://t.me/petercord\n*Telegram Penulis:* https://t.me/diemmmmmmmmmm' , MessageType.text);
+            await message.client.sendMessage(message.jid,'```AKU ADALAH USERBOT. 🐅 ASENAMansiez Rindu Itu berat!```\n\n*Version:* ```'+Config.VERSION+'```\n*Branch:* ```'+Config.BRANCH+'```\n*Telegram Group:* https://t.me/petercord\n*Telegram Penulis:* https://t.me/diemmmmmmmmmm' , MessageType.text);
         }
         else {
             await message.client.sendMessage(message.jid,Config.ALIVEMSG + '\n*Powered by WhatsAsena*', MessageType.text);
