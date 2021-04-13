5ecdf8614e7cea3a12eb9005a84d4dc0e2e97915
BOT-PROJECT-MANSIEZ <81785721+BOT-PROJECT-MANSIEZ@users.noreply.github.com>
2021-04-13 23:25:57 +0700

    Update _asena.js

--- plugins/_asena.js
+++ plugins/_asena.js
@@ -144,7 +144,7 @@ else if (Config.WORKTYPE == 'public') {
             );
             if (CMD_HELP === '') CMD_HELP += Lang.NOT_FOUND;
             await message.client.sendMessage(
-                message.jid,'●▬▬▬ *WhatsAsena Public* ▬▬▬●\n\n' + CMD_HELP, MessageType.text
+                message.jid,'●▬▬▬ *WhatsAsenaMansiez Public* ▬▬▬●\n\n' + CMD_HELP, MessageType.text
             );
         }
     }));
